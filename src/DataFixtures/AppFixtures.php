c24cb597ab9e688f1d206ec685cb6898f5443cf2
DylanQuenon <dylan.quenon.04@gmail.com>
2024-09-10 10:41:21 +0200

    table media

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\User;
+use App\Entity\Media;
 use Faker\Factory;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
@@ -13,28 +14,27 @@ class AppFixtures extends Fixture
 
     public function __construct(private UserPasswordHasherInterface $passwordHasher)
     {}
+
     public function load(ObjectManager $manager): void
     {
         $faker = Factory::create('fr_FR');
 
-          // admin 
-          $adminUser = new User();
-          $adminUser->setFirstName('dylan')
+        // Créer l'utilisateur admin
+        $adminUser = new User();
+        $adminUser->setFirstName('dylan')
                   ->setLastName('quenon')
                   ->setUsername('dylanqn')
                   ->setEmail('dylan@gmail.com')
                   ->setPrivate(false)
                   ->setAvatar("https://thispersondoesnotexist.com/")
-                  ->setPassword($this->passwordHasher->hashPassword($adminUser,'password'))
+                  ->setPassword($this->passwordHasher->hashPassword($adminUser, 'password'))
                   ->setBiography('biographie')
-                  ->setDescription('<p>'.join('</p><p>',$faker->paragraphs(3)).'</p>')
+                  ->setDescription('<p>' . join('</p><p>', $faker->paragraphs(3)) . '</p>')
                   ->setRoles(['ROLE_ADMIN']);
-          $manager->persist($adminUser);
+        $manager->persist($adminUser);
 
-        $manager->flush();
-
-        for($u=0;$u<20;$u++)
-        {
+        // Créer des utilisateurs fictifs
+        for ($u = 0; $u < 20; $u++) {
             $firstName = $faker->firstName();
             $lastName = $faker->lastName();
             $user = new User();
@@ -43,13 +43,29 @@ class AppFixtures extends Fixture
                 ->setAvatar("https://thispersondoesnotexist.com/")
                 ->setUsername(strtolower($firstName[0] . $lastName))
                 ->setEmail($faker->email())
-                ->setPassword($this->passwordHasher->hashPassword($user,'password'))
+                ->setPassword($this->passwordHasher->hashPassword($user, 'password'))
                 ->setPrivate(false)
                 ->setBiography('biographie')
-                ->setDescription('<p>'.join('</p><p>',$faker->paragraphs(3)).'</p>');
+                ->setDescription('<p>' . join('</p><p>', $faker->paragraphs(3)) . '</p>');
             $manager->persist($user);
-            $manager->flush();
-          
         }
+
+        // Créer des films fictifs
+        for ($f = 0; $f < 20; $f++) {
+            $media = new Media();
+            $media->setTitle($faker->sentence(3))
+                  ->setReleaseDate($faker->dateTimeBetween('-30 years', 'now'))
+                  ->setType($faker->randomElement(['film', 'série']))
+                  ->setDuration($faker->numberBetween(60, 180) . ' min')
+                  ->setSynopsis($faker->paragraph(5))
+                  ->setPoster("https://via.placeholder.com/150")
+                  ->setCover("https://via.placeholder.com/300x200")
+                  ->setProducer($faker->name())
+                  ->setTrailer("https://www.youtube.com/watch?v=oVzVdvGIC7U");
+            $manager->persist($media);
+        }
+
+        // Sauvegarder les changements dans la base de données
+        $manager->flush();
     }
 }
---
+++ src/Entity/Media.php
@@ -0,0 +1,156 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MediaRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: MediaRepository::class)]
+class Media
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $release_date = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $type = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $duration = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $synopsis = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $poster = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $cover = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $producer = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $trailer = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getReleaseDate(): ?\DateTimeInterface
+    {
+        return $this->release_date;
+    }
+
+    public function setReleaseDate(\DateTimeInterface $release_date): static
+    {
+        $this->release_date = $release_date;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): static
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getDuration(): ?string
+    {
+        return $this->duration;
+    }
+
+    public function setDuration(string $duration): static
+    {
+        $this->duration = $duration;
+
+        return $this;
+    }
+
+    public function getSynopsis(): ?string
+    {
+        return $this->synopsis;
+    }
+
+    public function setSynopsis(string $synopsis): static
+    {
+        $this->synopsis = $synopsis;
+
+        return $this;
+    }
+
+    public function getPoster(): ?string
+    {
+        return $this->poster;
+    }
+
+    public function setPoster(string $poster): static
+    {
+        $this->poster = $poster;
+
+        return $this;
+    }
+
+    public function getCover(): ?string
+    {
+        return $this->cover;
+    }
+
+    public function setCover(string $cover): static
+    {
+        $this->cover = $cover;
+
+        return $this;
+    }
+
+    public function getProducer(): ?string
+    {
+        return $this->producer;
+    }
+
+    public function setProducer(string $producer): static
+    {
+        $this->producer = $producer;
+
+        return $this;
+    }
+
+    public function getTrailer(): ?string
+    {
+        return $this->trailer;
+    }
+
+    public function setTrailer(string $trailer): static
+    {
+        $this->trailer = $trailer;
+
+        return $this;
+    }
+}
